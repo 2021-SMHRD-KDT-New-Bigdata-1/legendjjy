232b31d2bd93f4970f62d2f96726fa9c5ebd6c64
smhrd <smhrd@121.179.7.87>
2021-09-28 10:11:14 +0900

    dao 오류 커밋

--- Project1/src/main/java/comDAO/usersDAO.java
+++ Project1/src/main/java/comDAO/usersDAO.java
@@ -16,9 +16,9 @@ public class usersDAO {
 	public void conn() {
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			String url = "jdbc:oracle:thin:@localhost:1521:xe";
-			String dbid = "hr";
-			String dbpw = "hr";
+			String url = "jdbc:oracle:thin:@project-db-stu.ddns.net:1524:xe";
+			String dbid = "cgi_6_1";
+			String dbpw = "smhrd1";
 			conn = DriverManager.getConnection(url, dbid, dbpw);
 		} catch (Exception e) {
 			e.printStackTrace();
