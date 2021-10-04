cadf20fa1748ad8ceaeffb78f96e3fe09ca9643e
smhrd <smhrd@211.107.188.139>
2021-10-04 15:42:45 +0900

    푸시

--- Project1/src/main/java/comSERVLET/DeleteFollow.java
+++ Project1/src/main/java/comSERVLET/DeleteFollow.java
@@ -11,8 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import comDAO.usersDAO;
 
-@WebServlet("/DeleteApplication")
-public class DeleteApplication extends HttpServlet {
+@WebServlet("/DeleteFollow")
+public class DeleteFollow extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -20,15 +20,15 @@ public class DeleteApplication extends HttpServlet {
 		  request.setCharacterEncoding("EUC-KR");
 	      response.setCharacterEncoding("EUC-KR");
 		  PrintWriter out = response.getWriter() ; 
-		  int app_seq =  Integer.parseInt(request.getParameter("app_seq"));
+		  int follow_seq =  Integer.parseInt(request.getParameter("follow_seq"));
 		  
 	      usersDAO dao = new usersDAO();
-	      int cnt = dao.delete_Applist(app_seq);
+	      int cnt = dao.delete_follow(follow_seq);
 	      
 	      if(cnt>0) {
 	    	  out.print("<script>");
-	    	  out.print("alert('확인완료.')");
-	    	  out.print("location.href='history.back()");
+	    	  out.print("alert('삭제되었습니다.')");
+	    	  out.print("location.href='history.back()'");
 	    	  out.print("</script>");
 	      };
 	      
